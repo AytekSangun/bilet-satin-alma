a1c9dfd1fd7013d7986782c200a9c5fbae2ed8da
AytekSangun <ayteksn@gmail.com>
2025-10-22 16:56:52 +0300

    database oluşturma, temel işlevler

--- public/index.php
+++ public/index.php
@@ -1,3 +1,28 @@
 <?php
-phpinfo();
-?>
+
+session_start();
+
+require_once __DIR__ . '/../src/core/Database.php';
+require_once __DIR__ . '/../src/controllers/HomeController.php';
+require_once __DIR__ . '/../src/controllers/TripController.php';
+
+$requestUri = $_SERVER['REQUEST_URI'];
+$requestUri = strtok($requestUri, '?');
+
+switch ($requestUri) {
+    case '/':
+    case '/index.php':
+        $controller = new \App\Controllers\HomeController();
+        $controller->index();
+        break;
+
+    case '/trip-details':
+        $controller = new \App\Controllers\TripController();
+        $controller->showDetails();
+        break;
+
+    default:
+        http_response_code(404);
+        echo "<h1>404 Sayfa Bulunamadı</h1>";
+        break;
+}
